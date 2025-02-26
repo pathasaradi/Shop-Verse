303bdff92a51d6ef1d896b365196498c85644ac9
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2025-02-26 07:32:57 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,4 @@
 
 ## this is not required until apache config changed, just adding this to demonstrate the appspecfile
 echo "starting httpd service"
-sudo yum update -y
-sudo yum install -y httpd
 sudo systemctl start httpd
-sudo systemctl enable httpd
